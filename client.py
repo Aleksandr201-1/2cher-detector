16cdb9b297ff884384d49825f3f8902f64f84861
Aleksandr201-1 <70757686+Aleksandr201-1@users.noreply.github.com>
2023-12-26 03:44:04 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 
 @app.route('/main/', methods=['post', 'get'])
 def check():
+    user_id = 1
     list_of_hate = []
     sentence = ''
-    responce = requests.get('http://localhost:8085/check', data={'sentence':sentence})
+    responce = ('', 204)
     if request.method == 'POST':
         sentence = request.form.get('sentence')
-        responce = requests.get('http://localhost:8085/check', data={'sentence':sentence})
+        responce = requests.get('http://localhost:8085/check', data={'sentence':sentence, 'user_id':user_id})
     
     for val in responce.json().values():
         for v in val:
